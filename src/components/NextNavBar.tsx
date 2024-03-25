2edf3ee656c559c8b5bcedb1834806a982053a71
Santiago-Bernal <stinjaca@uninorte.edu.co>
2024-03-25 09:41:30 -0500

    Mejoras en la barra de navegacion

--- src/components/NextNavBar.tsx
+++ src/components/NextNavBar.tsx
@@ -1,19 +1,30 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
 
 export default function NextNavBar({ header }: { header: string }) {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   return (
     <Navbar
-        position="static"
-        className="border-b-[#C54108] border-b-1"
-        isMenuOpen={isMenuOpen}
-        onMenuOpenChange={setIsMenuOpen}
+      position="static"
+      className="border-b-[#C54108] border-b-1"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      maxWidth="2xl"
     >
-
       <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-        
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        />
       </NavbarContent>
 
       <NavbarContent className="sm:hidden pr-3" justify="center">
@@ -22,28 +33,45 @@ export default function NextNavBar({ header }: { header: string }) {
         </NavbarBrand>
       </NavbarContent>
 
-        <NavbarBrand className="hidden sm:flex pr-3">
-          <p className="font-bold text-[#C54108] text-lg">DevPassionate</p>
-        </NavbarBrand>
+      <NavbarBrand className="hidden sm:flex pr-3">
+        <p className="font-bold text-[#C54108] lg:text-lg text-sm">DevPassionate</p>
+      </NavbarBrand>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#about" className="hover:text-[#C54108] text-lg">
-            ABOUT ME
-          </Link>
-        </NavbarItem>
-        <NavbarItem >
-          <Link href="#skills" color="foreground" aria-current="page" className="hover:text-[#C54108] text-lg">
-            SKILLS
+          <Link
+            color="foreground"
+            href="#about"
+            className="hover:text-[#C54108] lg:text-lg text-sm"
+          >
+            SOBRE MI
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#portfolio" className="hover:text-[#C54108] text-lg">
+          <Link
+            href="#skills"
+            color="foreground"
+            aria-current="page"
+            className="hover:text-[#C54108] lg:text-lg text-sm"
+          >
+            HABILIDADES
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href="#portfolio"
+            className="hover:text-[#C54108] lg:text-lg text-sm"
+          >
             PORTFOLIO
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#" className="hover:text-[#C54108] text-lg">
+          <Link
+            color="foreground"
+            href="#"
+            className="hover:text-[#C54108] lg:text-lg text-sm"
+          >
             BLOG
           </Link>
         </NavbarItem>
@@ -51,9 +79,19 @@ export default function NextNavBar({ header }: { header: string }) {
 
       <NavbarContent justify="end">
         <NavbarItem>
-          <Button as={Link} color="primary" className="bg-[#C54108] text-white" href="#" variant="flat">
-            <a href="https://publuu.com/flip-book/409469/925708" download="curriculum.pdf" target="_blank">
-              Download CV
+          <Button
+            as={Link}
+            color="primary"
+            className="bg-[#C54108] text-white lg:text-lg text-sm"
+            href="#"
+            variant="flat"
+          >
+            <a
+              href="https://online.pubhtml5.com/uywum/xoec/"
+              download="curriculum.pdf"
+              target="_blank"
+            >
+              Currículum Vitae
             </a>
           </Button>
         </NavbarItem>
@@ -61,42 +99,64 @@ export default function NextNavBar({ header }: { header: string }) {
 
       <NavbarMenu>
         <NavbarMenuItem>
-            <Link className="w-full" color="foreground" href="/about" size="lg">
-              ABOUT ME
-            </Link>
+          <Link className="w-full" color="foreground" href="/about" size="lg">
+            SOBRE MI
+          </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-            <Link className="w-full" color="foreground" href="#" size="lg">
-              SKILLS
-            </Link>
+          <Link className="w-full" color="foreground" href="#" size="lg">
+            HABILIDADES
+          </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-            <Link className="w-full" color="foreground" href="/proyects" size="lg">
-              PORTFOLIO
-            </Link>
+          <Link
+            className="w-full"
+            color="foreground"
+            href="/proyects"
+            size="lg"
+          >
+            PORTFOLIO
+          </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-            <Link className="w-full" color="foreground" href="#" size="lg">
-              BLOG
-            </Link>
+          <Link className="w-full" color="foreground" href="#" size="lg">
+            BLOG
+          </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-            <Link className="w-full" color="foreground" target="_blank" href="https://github.com/SantiagoBer13" size="lg">
-              Git Hub
-            </Link>
+          <Link
+            className="w-full"
+            color="foreground"
+            target="_blank"
+            href="https://github.com/SantiagoBer13"
+            size="lg"
+          >
+            GIT HUB
+          </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-            <Link className="w-full" color="foreground" target="_blank" href="https://www.linkedin.com/in/santiago-bernal-tinjaca/" size="lg">
-              LinkedIn
-            </Link>
+          <Link
+            className="w-full"
+            color="foreground"
+            target="_blank"
+            href="https://www.linkedin.com/in/santiago-bernal-tinjaca/"
+            size="lg"
+          >
+            LINKEDIN
+          </Link>
         </NavbarMenuItem>
         <NavbarMenuItem>
-            <Link className="w-full" color="foreground" target="_blank" href="https://www.instagram.com/devpassionate/" size="lg">
-              Instagram
-            </Link>
+          <Link
+            className="w-full"
+            color="foreground"
+            target="_blank"
+            href="https://www.instagram.com/devpassionate/"
+            size="lg"
+          >
+            INSTAGRAM
+          </Link>
         </NavbarMenuItem>
       </NavbarMenu>
-
     </Navbar>
   );
 }
